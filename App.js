b45f87dddf1249bc844026531724f49c07e6d158
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2022-10-04 16:41:43 +0100

    GET

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect} from 'react'
+import { Text, View, ScrollView, StyleSheet, TouchableOpacity, TextInput, SafeAreaView, Modal } from 'react-native'
+import axios from "axios";
 
-export default function App() {
+
+const App = () => {
+
+
+  const [List,setList] = useState([]);
+  const [Visible,setVisible] = useState(false);
+
+  //ComponentDidMount :
+
+  useEffect(()=> {
+    getList()
+  },[]);
+
+
+  const getList = () => {
+    axios({
+      url: "https://nitc.cleverapps.io/api/courses",
+      method: "GET"
+    })
+    .then((response) => {
+      console.log(response);
+      var res = response.data;
+      setList(response.data);
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+ 
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <ScrollView>
+        {List.map((item,index)=> {
+          return (
+            <View style={styles.item_course} key={index}>
+              <View>
+              <Text style={styles.txt_name}>{index+1}. {item.name}</Text>
+              <Text style={styles.txt_item}>{item.price}</Text>
+              <Text style={styles.txt_item}>{item.description}</Text>
+              <Text style={item.status === 1 ? styles.txt_enabled : styles.txt_disabled}>{item.status === 1 ? "Enabled" : "Disabled"}</Text>
+              </View>
+              {/* <View>
+                <TouchableOpacity
+                onPress={() => handleDelete(item)}
+                >
+                  <Text style={styles.txt_del}> Delete </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                onPress={() => handleEdit(item)}
+                >
+                  <Text style={styles.txt_edit}> Edit </Text>
+                </TouchableOpacity>
+              </View> */}
+            </View>
+          )
+        })
+
+        }
+      </ScrollView>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
+
+const styles= StyleSheet.create({
+
+})
